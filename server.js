bf735b61a6f6e28dd4d99ede65b6127272062e2f
Stephen Fike <sfike98@gmail.com>
2022-04-18 21:02:01 -0500

    added mysql and integrated database

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mysql = require('mysql2');
+
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +8,18 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username
+        user: 'root',
+        // Your MySQL password
+        password: '$Fike1129SQL!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 // Default response for any Not Found content
 app.use((req, res) => {
